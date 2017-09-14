39e86bf99f87a5aec20191d0ee5d7d8aba1f4830
sunsakuar <zhuruyi9513@163.com>
2017-09-14 15:00:37 +0800

    learn spring

---
+++ src/main/java/net/zhuruyi/Controller/HelloController.java
@@ -0,0 +1,33 @@
+package net.zhuruyi.Controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller {
+
+    private static final Log logger = LogFactory.getLog(HelloController.class);
+
+
+    /**
+     * HandleRequest是Controller接口必须实现的方法 该方法的参数是对应请求HttpServletRequest和HttpServletResponse
+     * 该方法必须放回一个包含视图名和模型的ModelAndView对象
+     */
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        logger.info("HandlerRequest被调用");
+        //创建准备返回的ModelAndView对象，该对象通常包含了返回视图名/模型的名称以及模型的对象
+        ModelAndView modelAndView = new ModelAndView();
+        //添加模型数据，可以是任意的POJO对象
+        modelAndView.addObject("message", "HelloWorld");
+        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+        modelAndView.setViewName("/WEB-INF/content/welcome.jsp");
+        //返回ModelAndView对象
+        return modelAndView;
+        
+    }
+}
---
+++ src/main/webapp/WEB-INF/content/Welcome.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhuru
+  Date: 2017/9/14
+  Time: 14:02
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <mate http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+    <title>Welcome</title>
+</head>
+<body>
+${requestScope.message}
+</body>
+</html>
