c1df4d5c5646b688175782fe72f20c1f21b721c8
sunsakuar <zhuruyi9513@163.com>
2017-09-14 15:49:25 +0800

    修改url错误后提交

--- src/main/java/net/zhuruyi/Controller/HelloController.java
+++ src/main/java/net/zhuruyi/Controller/HelloController.java
@@ -17,17 +17,22 @@ public class HelloController implements Controller {
      * HandleRequest是Controller接口必须实现的方法 该方法的参数是对应请求HttpServletRequest和HttpServletResponse
      * 该方法必须放回一个包含视图名和模型的ModelAndView对象
      */
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+
         logger.info("HandlerRequest被调用");
         //创建准备返回的ModelAndView对象，该对象通常包含了返回视图名/模型的名称以及模型的对象
         ModelAndView modelAndView = new ModelAndView();
-        //添加模型数据，可以是任意的POJO对象
-        modelAndView.addObject("message", "HelloWorld");
-        //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
-        modelAndView.setViewName("/WEB-INF/content/welcome.jsp");
-        //返回ModelAndView对象
-        return modelAndView;
-        
+        try {
+            //添加模型数据，可以是任意的POJO对象
+            modelAndView.addObject("message", "HelloWorld");
+            //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面
+            modelAndView.setViewName("/WEB-INF/content/Welcome.jsp");
+            //返回ModelAndView对象
+            return modelAndView;
+        } catch (Exception e) {
+            System.out.println("Error:" + e);
+        }
+        return null;
+
     }
 }
